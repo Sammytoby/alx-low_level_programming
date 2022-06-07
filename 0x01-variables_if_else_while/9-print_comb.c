eee544632cd9a8c3b826cc03b4b0f3c119e8fa72
Sammytoby <madukatobechukwusamuel@gmail.com>
2022-06-06 21:11:34 -0700

    Project C variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+	int comb;
+
+	for (comb = '0'; comb <= '9'; comb++)
+	{
+	putchar(comb);
+	if (com  < '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
