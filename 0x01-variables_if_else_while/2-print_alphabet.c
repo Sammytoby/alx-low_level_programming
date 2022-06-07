87e5a0cd9e3c68d79ab8204d1aaf879757809064
Sammytoby <madukatobechukwusamuel@gmail.com>
2022-06-06 20:20:28 -0700

    Project C variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; ++az)
+	{
+		putchar(az);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
